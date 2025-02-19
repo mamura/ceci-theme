39bd0cc98c06450592f1b880a4ad9a30b55fba89
Mamura <marcio.mota@gmail.com>
2025-02-19 16:47:04 -0300

    fix display body - CSS

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import "./assets/sass/main.scss";
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Signin from './pages/Users/Signin'
